c481142d330979f25be872b7ccc8739e47dd506c
Ali <45128741+Alioax@users.noreply.github.com>
2025-07-27 21:41:54 +0330

    Update run.py

--- api/run.py
+++ api/run.py
@@ -26,7 +26,7 @@ class handler(BaseHTTPRequestHandler):
             'password': 'o91759o9576'  # reddit's account password
         }
 
-        subreddit = "midjourney"
+        subreddit = "art"
         listing = "hot"  # listing category
         limit = "10"  # posts to check
 
